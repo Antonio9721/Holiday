664e247c2093247e130f9d2b3aebf4b619d1da1a
Antonio <57236660+Antonio9721@users.noreply.github.com>
2020-10-30 00:55:17 -0600

    Hoteles/Views
    Vistas del módulo de hoteles

--- edit.blade.php
+++ edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.layout')
 @extends('layout.nav')
-@section('title', 'Editar Atractivo')
+@section('title', 'Editar Registro')
 @section('content')
 
 <center>
@@ -11,7 +11,7 @@
 
 <div class="row">
 	<div class="col-sn-2">
-	    <a href="{{ route('destinos.index') }}">
+	    <a href="{{ route('hotels.index') }}">
 	      <button class="btn btn-danger">Cancelar Edición</button>
 	  </a></div>
 </div>
@@ -19,53 +19,77 @@
 
 
 <div class="jumbotron">
-<form method="post" action="{{ route('destinos.update', $destino->id) }}">
+<form method="post" action="{{ route('hotels.update', $hotel->id) }}">
 	@csrf
 	@method('PUT')
 
 	<div>
-		<label for="">Atractivo Turístico:</label>
+		<label for="">Nombre del Hotel:</label>
 		<select class="form-control" name="nombre" id="" required>
-		  <option value="{{$destino->nombre }}">Seleccione un atractivo</option>
-		  <option value="Exconvento de Santa Mería de la Asunción">Exconvento de Santa Mería de la Asunción</option>
-		  <option value="Plaza Principal">Plaza Principal</option>
-		  <option value="Cerro Cabezón">Cerro Cabezón</option>
-		  <option value="Iglesia del Sagrado Corazón de Jesús">Iglesia del Sagrado Corazón de Jesús</option>
-		  <option value="Santuario del Señor de Huaxtla​a">Santuario del Señor de Huaxtla​</option>
-		  <option value="Presa de la soledad">Presa de la soledad</option>
-		  <option value="Cascada de Puxtla">Cascada de Puxtla</option>
-		  <option value="Cascada de Tenazate">Cascada de Tenazate</option>
-		  <option value="Manantiales de Xiliaco">Manantiales de Xiliaco</option>
-		  <option value="Cultivo de orquídeas">Cultivo de orquídeas</option>
+		  <option value="{{$hotel->nombre }}">--Seleccione un hotel--</option>
+		  <option value="">Seleccione un hotel</option>
+          <option value="Hotel Santa Fe">Hotel Santa Fe</option>
+          <option value="Hotel San Jorge">Hotel San Jorge</option>
+          <option value="JH Hotel Tlatlauquitepec">JH Hotel Tlatlauquitepec</option>
+          <option value="Hotel Suites Tlatlauquitepec">Hotel Suites Tlatlauquitepec</option>
+          <option value="Hotel Plaza Gloria">Hotel Plaza Gloria</option>
+          <option value="Hotel Posada Serrana">Hotel Posada Serrana</option>
+          <option value="Hostal Tlatlauquitepec">Hostal Tlatlauquitepec</option>
+          <option value="Casa Afif">Casa Afif</option>
 		</select>
 	</div>
 	<br>
 
 	<div>
-		<label for="">Ubicación del Atractivo:</label>
-		<input class="form-control" type="text" value="{{$destino->comunidad }}" name="comunidad" id="" required maxlength="25">
+		<label for="">Tipo de Habitación:</label>
+		<input class="form-control" type="text" value="{{$hotel->habitacion }}" name="habitacion" id="" required maxlength="25">
 	</div>
 	<br>
 
 
 	<div>
-		<label for="">Número de Visitantes:</label>
-		<input class="form-control"  type="int" value="{{$destino->visitantes }}" name="visitantes" id="" required maxlength="15">
+		<label for="">Número de la Habitación:</label>
+		<input class="form-control"  type="int" value="{{$hotel->nhabitacion }}" name="nhabitacion" id="" required maxlength="3">
 	</div>
 	<br>
 
 
 	<div>
-		<label for="">Fecha de la Visita:</label>
-		<input class="form-control" type="date" value="{{$destino->fecha }}" name="fecha" id="" required maxlength="">
+		<label for="">Tiempo de Estadia:</label>
+		<input class="form-control" type="text" value="{{$hotel->tiempo }}" name="tiempo" id="" required maxlength="12">
 	</div>
 <br>
 
     <div>
-		<label for="">Hora de la Visita:</label>
-		<input class="form-control" type="time" value="{{$destino->tiempo }}" name="tiempo" id="" required maxlength="">
+		<label for="">Precio de la Reservación:</label>
+		<input class="form-control" type="text" value="{{$hotel->precio }}" name="precio" id="" required maxlength="16">
 	</div>
 <br>
+
+    <div>
+		<label for="">Fecha de la Reservación:</label>
+		<input class="form-control" type="date" value="{{$hotel->fecha }}" name="fecha" id="" required maxlength="">
+	</div>
+<br>
+
+    <div>
+		<label for="">Hora de la Reservación:</label>
+		<input class="form-control" type="time" value="{{$hotel->hora }}" name="" id="hora" required maxlength="">
+	</div>
+<br>
+
+     <div>
+		<label for="">Número de Huespedes:</label>
+		<input class="form-control" type="int" value="{{$hotel->npersonas }}" name="npersonas" id="" required maxlength="">
+	</div>
+<br>
+
+	<div>
+		<label for="">Número Teléfonico:</label>
+		<input class="form-control" type="int" value="{{$hotel->telefono }}" name="telefono" id="" required maxlength="">
+	</div>
+<br>
+
 	<input class="btn btn-primary" type="submit" value="Guardar Cambios">
 	
 </form>
--- index.blade.php
+++ index.blade.php
@@ -1,35 +1,35 @@
 @extends('layout.layout')
 @extends('layout.nav')
-@section('title', 'Destinos')
+@section('title', 'Hotels')
 @section('content')
 
 <center>
   <div class="col-sn-10"><h3>Tlatlauquitepec Pueblo Mágico</h3></div>
-  <div class="col-sn-10"><h4>Atractivos Turísticos</h4></div>
+  <div class="col-sn-10"><h4>Reservaciones Hoteleras</h4></div>
   </center>
 <div class="row">
-	<div class="col-sn-2"><a class="btn btn-primary" href="{{ route('destinos.create') }}">Agregar Destino</a></div>
+	<div class="col-sn-2"><a class="btn btn-primary" href="{{ route('hotels.create') }}">Agregar Reservación</a></div>
 </div>
 <br>
 
-@forelse($destinos as $destino)
+@forelse($hotels as $hotel)
 
 <center>
   <div class="card mb-3"  style="max-width: 1020px;">
   <div class="row no-gutters">
     <div class="col-md-4">
-      <img src="https://www.corazondepuebla.com.mx/wp-content/uploads/2018/10/tlatauquitetpec-pueblo-magico-pueblam-exico-06.jpg" width="336" height="286">
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/e9/7a/cc/hotel-santa-fe.jpg?w=600&h=300&s=1" width="336" height="286">
     </div>
     <div class="col-md-8 ">
       <div class="card-body bg-dark text-white text-center">
-        <h5 class="card-header bg-info text-center"><b>{{ $destino->nombre }}</b></h5>
-        <p class="card-text"><b>Comunidad: </b>{{ $destino->comunidad }}</p>
-            <p class="card-text"> <b> Número de Visitantes:</b> {{ $destino->visitantes }}</p>
-            <p class="card-text"><b>Fecha de la Visita:</b> {{ $destino->fecha }} <b> Hora de la Visita:</b> {{ $destino->tiempo }}</p>
-            <p class="card-text"><b> Hora de la Visita:</b> {{ $destino->tiempo }}</p>
-        <form action="{{ route('destinos.destroy', $destino->id) }}" method="post">
-      <a type="submit" class="btn btn-success" href="{{ route('destinos.show', $destino->id) }}">Ver</a>
-      <a type="submit" class="btn btn-primary" href="{{ route('destinos.edit', $destino->id) }}">Editar</a>
+        <h5 class="card-header bg-info text-center"><b>{{ $hotel->nombre }}</b></h5>
+        <p class="card-text"><b>Tipo de Habitación:</b> {{ $hotel->habitacion }} <b>No. Habitación:</b> {{ $hotel->nhabitacion }}</p>
+            <p class="card-text"><b>Tiempo de Estadia:</b> {{ $hotel->tiempo }} <b> Precio: </b> {{ $hotel->precio }}</p>
+            <p class="card-text"><b>Fecha de Reservación: </b> {{ $hotel->fecha }} <b>Hora de Reservación: </b> {{ $hotel->hora }}</p>
+            <p class="card-text"><b>Número de Huespedes: </b> {{ $hotel->npersonas }} <b>Númro Teléfonico: </b> {{ $hotel->telefono }}</p>
+        <form action="{{ route('hotels.destroy', $hotel->id) }}" method="post">
+      <a type="submit" class="btn btn-success" href="{{ route('hotels.show', $hotel->id) }}">Ver</a>
+      <a type="submit" class="btn btn-primary" href="{{ route('hotels.edit', $hotel->id) }}">Editar</a>
        @csrf
       @method('DELETE')
       <button type="submit" class="btn btn-danger">Eliminar</button>
@@ -44,11 +44,11 @@
 <br>
 
 @empty
-<div><h3>No hay destinos registrados en la base de datos</h3></div>
+<div><h3>No hay reservaciones registradas en la base de datos</h3></div>
 @endforelse
 
 <center>
-{{$destinos->links() }}
+{{$hotels->links() }}
 </center>
 
 @endsection
